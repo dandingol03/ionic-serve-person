b2c5d29c9321ed9af0679451aeab966f109d870e
danding <1291248710@qq.com>
2016-09-23 20:09:25 +0800

    dw
    dw

---
+++ www/views/login/login.js
@@ -0,0 +1,41 @@
+/**
+ * Created by danding on 16/9/6.
+ */
+angular.module('starter')
+
+  .controller('loginController',function($scope,$state,$ionicLoading,$http){
+
+    $scope.user={};
+
+    //登录
+    $scope.login = function(){
+
+      $http({
+        method:"POST",
+        data:"grant_type=password&password=" + $scope.user.password + "&username=" + $scope.user.username,
+        url:"proxy/node_server/login",
+        headers: {
+          'Authorization': "Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW",
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+      }).success(function(response){
+
+        var access_token=response.access_token;
+
+
+      }).error(function(err){
+        var error='';
+        for(var field in err)
+        {
+          error+=err[field]+'\r\n';
+        }
+        alert('error=' + error);
+      });
+
+    }
+
+
+
+
+  });
