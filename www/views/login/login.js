e90cf27101422ea09ae12349d8ca7f754a7aa615
danding <1291248710@qq.com>
2016-09-30 08:48:57 +0800

    xx

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -7,8 +7,84 @@ angular.module('starter')
 
     $scope.user={};
 
+
+
+
+    $scope.tt=function(){
+      $http({
+        method:"POST",
+        data:"grant_type=password&password=" + $scope.user.password + "&username=" + $scope.user.username,
+        url:"http://202.194.14.106:3000/login",
+        headers: {
+          'Authorization': "Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW",
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).success(function(res) {
+        var access_token=res.access_token;
+        alert('access_token=' + access_token);
+      }).error(function(err) {
+        alert('error=\r\n' + err.toString());
+      })
+    }
+
+    $scope.login=function(){
+      //$http({
+      //  method:"POST",
+      //  data:"grant_type=password&password=" + $scope.user.password + "&username=" + $scope.user.username,
+      //  url:"http://192.168.1.106:3000/login",
+      //  headers: {
+      //    'Authorization': "Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW",
+      //    'Content-Type': 'application/x-www-form-urlencoded'
+      //  }
+      //}).success(function(res) {
+      //  var access_token=res.access_token;
+      //  alert('access_token=' + access_token);
+      //}).error(function(err) {
+      //  alert('error=\r\n' + err.toString());
+      //});
+
+      $http({
+        method:"POST",
+        data:"grant_type=password&password=" + $scope.user.password + "&username=" + $scope.user.username,
+        url:"http://192.168.1.106:3000/login",
+        headers: {
+          'Authorization': "Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW",
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function(res) {
+        var json=res.data;
+        $rootScope.access_token=json.access_token;
+        if(json.access_token!==undefined&&json.access_token!==null)
+        {
+          return   $http({
+            method: "post",
+            url: "http://192.168.1.106:3000/svr/request",
+            headers: {
+              'Authorization': "Bearer " + $rootScope.access_token,
+            },
+            data:
+            {
+              request:'activatePersonOnline',
+              info:{
+                registrationId:$rootScope.registrationId
+              }
+            }
+          });
+        }
+      }).then(function(json) {
+        alert('got it ');
+      }).catch(function(err) {
+        var str='';
+        for(var field in err)
+          str+=err[field];
+        alert('error=' + str);
+      });
+
+
+    }
+
     //登录
-    $scope.login = function(){
+    $scope.login_backup = function(){
 
       $http({
         method:"POST",
@@ -19,12 +95,12 @@ angular.module('starter')
           'Content-Type': 'application/x-www-form-urlencoded'
         }
       }).then(function(response){
-
+        alert('request is back');
         $rootScope.access_token=response.data.access_token;
-        alert('access_token=' + $rootScope.access_token);
+        alert('access_token=\r\n' + response.data.access_token);
         return   $http({
           method: "post",
-          url: "http://192.168.1.106:3000/svr/request",
+          url: "/proxy/node_server/svr/request",
           headers: {
             'Authorization': "Bearer " + $rootScope.access_token,
           },
@@ -42,15 +118,11 @@ angular.module('starter')
         {
 
         }
-        $state.go('tabs.dashboard');
+        $state.go('register');
       }).catch(function(err){
         var error='';
-        for(var field in err)
-        {
-          error+=err[field]+'\r\n';
-        }
-        alert('error=' + error);
-        $state.go('tabs.dashboard');
+        alert('error=' + err.toString());
+
       });
 
     }
