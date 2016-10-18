c2cada5a735c2111e5bb76896f05be69b17fa9b0
EEvvee <dingyiming_sdu@163.com>
2016-10-18 20:58:51 +0800

    xx

--- www/js/app.js
+++ www/js/app.js
@@ -226,7 +226,7 @@ angular.module('starter', ['ionic','ngCordova'])
     var ob={
       local:function(){
         if(window.cordova!==undefined&&window.cordova!==null)
-          return "http://192.168.1.106:3000";
+          return "http://192.168.1.105:3000";
         else
           return "/proxy/node_server";
       }
