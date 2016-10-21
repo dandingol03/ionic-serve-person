318f43cf21d53fc422bb9a2c9282873a4ebfa56f
EEvvee <dingyiming_sdu@163.com>
2016-10-21 15:22:57 +0800

    xx

--- www/views/dashboard/dashboard.js
+++ www/views/dashboard/dashboard.js
@@ -33,6 +33,9 @@ angular.module('starter')
                            21:'车驾管-审车',22:'车驾管-审证',23:'车驾管-接送机',24:'车驾管-取送车',
                            31:'鈑喷'};
 
+    $scope.subServiceTypeMap={1:'机油,机滤',2:'检查制动系统,更换刹车片',3:'雨刷片更换',
+      4:'轮胎更换',5:'燃油添加剂',6:'空气滤清器',7:'检查火花塞',8:'检查驱动皮带',9:'更换空调滤芯',10:'更换蓄电池,防冻液'};
+
 
     $scope.page_orders=[[],[],[],[]];
     $scope.pageLimit=3;
@@ -57,6 +60,20 @@ angular.module('starter')
           $scope.orders[0]=json.data;
           $scope.allOrders.map(function(order,i) {
             order.serviceName=$scope.serviceTypeMap[order.serviceType];
+            if(order.subServiceTypes!=null){
+              var subServiceTypes=order.subServiceTypes;
+              var types=subServiceTypes.split(',');
+              var serviceContent=[];
+              types.map(function(type,i) {
+                serviceContent.push($scope.subServiceTypeMap[type]);
+              });
+              order.subServiceContent=serviceContent;
+            }
+
+            var date=new Date(order.estimateTime);
+            order.time=date.getFullYear().toString()+'-'
+              +date.getMonth().toString()+'-'+date.getDate().toString();
+
             if(order.serviceType=='11'||order.serviceType=='12'||order.serviceType=='13')
               $scope.orders[1].push(order);
             if(order.serviceType=='21'||order.serviceType=='22'||order.serviceType=='23'||order.serviceType=='24')
