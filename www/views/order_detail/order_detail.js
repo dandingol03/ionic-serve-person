b3bb2d8786de20ef79f7d1dd40cf1a51f7bc2b74
EEvvee <dingyiming_sdu@163.com>
2016-10-26 16:46:15 +0800

    xx

--- www/views/order_detail/order_detail.js
+++ www/views/order_detail/order_detail.js
@@ -37,15 +37,13 @@ angular.module('starter')
           alert('urlAddress' + json.data.urlAddress);
 
           var url = Proxy.local() + '/svr/request?request=downloadAttachment' + '&urlAddress=' + json.data.urlAddress;
-          //var filesystem = cordova.file.documentsDirectory;
-          var filesystem = cordova.file.applicationStorageDirectory;
+          var filesystem = cordova.file.documentsDirectory;
           var prefixIndex = filesystem.indexOf('/Application');
           $scope.target = 'cdvfile://localhost/persistent/' + 'test.mp3';
 
           $scope.filepath=filesystem+'test.mp3';
           //var targetPath='cdvfile://localhost/persistent/Application/2AF47566-EE4A-41A8-94F5-73ED11427A80/ionic-serve-person.app/test.caf';
           alert('target path=\r\n' + $scope.target);
-          alert('filepath=\r\n' + $scope.filepath);
 
           var trustHosts = true;
           var options = {
@@ -55,11 +53,10 @@ angular.module('starter')
             }
           };
           $cordovaFileTransfer.download(url, $scope.target, options, trustHosts)
-            .then(function (result) {
-              for(var field in result)
-              {
-                alert('field=' + field + '\r\n' + result[field]);
-              }
+            .then(function (res) {
+              var json=res.response;
+              if(Object.prototype.toString.call(json)=='[object String]')
+                json=JSON.parse(json);
               alert('success');
             }, function (err) {
               // Error
@@ -140,70 +137,73 @@ angular.module('starter')
 
     //愿意接单
     $scope.takeOrder = function(){
-      var servicePersonId=null;
-      var unit=null;
-      var servicePersonName=null;
-      $http({
-        method: "post",
-        url:Proxy.local()+"/svr/request",
-        headers: {
-          'Authorization': "Bearer " + $rootScope.access_token,
-        },
-        data:
-        {
-          request:'getServicePersonIdByPersonId'
 
-        }
-      }).then(function(res) {
-        var json=res.data;
-        if(json.re==1) {
-          servicePersonId=json.data;
+      //如果订单是被筛选出来的
+      if($scope.order.servicePersonId==null||$scope.order.servicePersonId==undefined){
+        var servicePersonId=null;
+        var unit=null;
+        var servicePersonName=null;
+        $http({
+          method: "post",
+          url:Proxy.local()+"/svr/request",
+          headers: {
+            'Authorization': "Bearer " + $rootScope.access_token,
+          },
+          data:
+          {
+            request:'getServicePersonIdByPersonId'
 
-          return  $http({
-            method: "post",
-            url:Proxy.local()+"/svr/request",
-            headers: {
-              'Authorization': "Bearer " + $rootScope.access_token,
-            },
-            data:
-            {
-              request:'getUnitByServicePerson',
-              info:{
-                servicePersonId:servicePersonId
+          }
+        }).then(function(res) {
+          var json=res.data;
+          if(json.re==1) {
+            servicePersonId=json.data;
+
+            return  $http({
+              method: "post",
+              url:Proxy.local()+"/svr/request",
+              headers: {
+                'Authorization': "Bearer " + $rootScope.access_token,
+              },
+              data:
+              {
+                request:'getUnitByServicePerson',
+                info:{
+                  servicePersonId:servicePersonId
+                }
               }
-            }
-          });
-        }
-      }).then(function(res) {
+            });
+          }
+        }).then(function(res) {
 
-        var json=res.data;
-        if(json.re==1) {
-          alert('send')
-          unit=json.data;
-          var mobilePhone=null;
-          if(unit.mobilePhone!==undefined&&unit.mobilePhone!==null)
-            mobilePhone=unit.mobilePhone;
-          else if(unit.phone!==undefined&&unit.phone!==null)
-            mobilePhone=unit.phone;
-          return  $http({
-            method: "post",
-            url:Proxy.local()+"/svr/request",
-            headers: {
-              'Authorization': "Bearer " + $rootScope.access_token
-            },
-            data:
-            {
-              request:'sendCustomMessage',
-              info:{
-                unitName:unit.unitName,
-                mobilePhone:mobilePhone,
-                type:'to-customer',
-                order:$scope.order
+          var json=res.data;
+          if(json.re==1) {
+            alert('send')
+            unit=json.data;
+            var mobilePhone=null;
+            if(unit.mobilePhone!==undefined&&unit.mobilePhone!==null)
+              mobilePhone=unit.mobilePhone;
+            else if(unit.phone!==undefined&&unit.phone!==null)
+              mobilePhone=unit.phone;
+            return  $http({
+              method: "post",
+              url:Proxy.local()+"/svr/request",
+              headers: {
+                'Authorization': "Bearer " + $rootScope.access_token
+              },
+              data:
+              {
+                request:'sendCustomMessage',
+                info:{
+                  unitName:unit.unitName,
+                  mobilePhone:mobilePhone,
+                  type:'to-customer',
+                  order:$scope.order
+                }
               }
-            }
-          });
-        }
-      }).then(function(res) {
+            });
+          }
+        }).then(function(res) {
           var json=res.data;
           if(json.re==1){
 
@@ -226,20 +226,50 @@ angular.module('starter')
 
           }
 
-      }).then(function(res) {
+        }).then(function(res) {
 
-        if(res.re==1) {
-          console.log('service order has been generated');
-          $scope.go_back();
-        }
-      }).catch(function(err) {
-        var str='';
-        for(var field in err)
-          str+=err[field];
-        console.error(str);
-      });
+          if(res.re==1) {
+            console.log('service order has been generated');
+            $scope.go_back();
+          }
+        }).catch(function(err) {
+          var str='';
+          for(var field in err)
+            str+=err[field];
+          console.error(str);
+        });
+
+      }
+
+      //订单是被指派的
+      else{
+        $http({
+          method: "post",
+          url:Proxy.local()+"/svr/request",
+          headers: {
+            'Authorization': "Bearer " + $rootScope.access_token,
+          },
+          data:
+          {
+            request:'updateServiceOrderState',
+            info:{
+              orderState:2,
+              order:$scope.order,
+              msg:""
+
+            }
+          }
+        }).then(function(res) {
+
+          if(res.re==1) {
+            console.log('service order has been taken');
+            $scope.go_back();
+          }
+        })
+      }
 
     }
+
 
     $scope.changeOrderState = function(state){
       $http({
@@ -283,7 +313,6 @@ angular.module('starter')
        * file:///var/mobile/Containers/Data/Application/76687390-A99F-4220-9EB0-BB5A63154412/Documents/abc.caf
        */
 
-      alert('play');
       var filepath=$scope.filepath;
       filepath = filepath.replace('file://','');
       var media = $cordovaMedia.newMedia(filepath);
@@ -292,8 +321,68 @@ angular.module('starter')
         playAudioWhenScreenIsLocked : false
       }
       media.play(iOSPlayOptions); // iOS only!
+    }
 
+    $scope.readFile=function(fileEntry) {
 
+      fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+          console.log("Successful file read: " + this.result);
+          alert(fileEntry.fullPath + ": " + this.result);
+        };
+
+        reader.readAsText(file);
+
+      }, function(err){
+        alert('err=\r\n' + err);
+      });
+    }
+
+    $scope.writeFile=function(fileEntry, dataObj) {
+      // Create a FileWriter object for our FileEntry (log.txt).
+      fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+          console.log("Successful file write...");
+          readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+          console.log("Failed file write: " + e.toString());
+        };
+
+        // If data object is not passed in,
+        // create a new Blob instead.
+        if (!dataObj) {
+          dataObj = new Blob(['some file data'], { type: 'text/plain' });
+        }
+
+        fileWriter.write(dataObj);
+      });
+    }
+
+    $scope.createFile=function(dirEntry, fileName, isAppend) {
+      // Creates a new file or returns the file if it already exists.
+      dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+
+        $scope.writeFile(fileEntry, null, isAppend);
+
+      }, function(err){
+        alert('err=\r\n' + err);
+      });
+    }
+
+    $scope.createTF=function(){
+      window.requestFileSystem(window.TEMPORARY, 5 * 1024 * 1024, function (fs) {
+
+        alert('file system open: ' + fs.name);
+        $scope.createFile(fs.root, "newTempFile.txt", false);
+
+      }, function(err){
+        alert('err=\r\n' + err);
+      });
     }
 
 
