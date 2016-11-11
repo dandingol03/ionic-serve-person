9cd6fb613c6467da7bf0cb5523c1ad4b22d98b56
danding <1291248710@qq.com>
2016-11-11 17:06:16 +0800

    xx

--- www/js/app.js
+++ www/js/app.js
@@ -223,7 +223,7 @@ angular.module('starter', ['ionic','ngCordova'])
     var ob={
       local:function(){
         if(window.cordova!==undefined&&window.cordova!==null)
-          return "http://192.168.0.198:3000";
+          return "http://192.168.1.113:3000";
         else
           return "/proxy/node_server";
       }
